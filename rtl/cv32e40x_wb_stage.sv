f857f9c67e0073a29740bbd5616e6eaec4f53ead
jjones <jefferson.jones@tuwien.ac.at>
2025-08-07 13:23:32 +0200

    Add check for xif_result.we for register file writes

--- rtl/cv32e40x_wb_stage.sv
+++ rtl/cv32e40x_wb_stage.sv
@@ -109,8 +109,9 @@ module cv32e40x_wb_stage import cv32e40x_pkg::*;
   // In case of MPU/PMA error, the register file should not be written.
   // rf_we_wb_o is deasserted if lsu_mpu_status is not equal to MPU_OK
 
-  // TODO: Could use result interface.we into account if out of order completion is allowed.
-  assign rf_we_wb_o     = ex_wb_pipe_i.rf_we && !lsu_exception && !xif_waiting && !xif_exception && instr_valid;
+  // TODO: Could use result interface.we into account if out of order completion is allowed.  
+  // Add filter on regfile writes to not write if the result.we signal is low for offloaded instructions
+  assign rf_we_wb_o     = ex_wb_pipe_i.rf_we && !lsu_exception && !xif_waiting && !xif_exception && instr_valid && (!ex_wb_pipe_i.xif_en || (ex_wb_pipe_i.rf_we && xif_result_if.result.we));
   // TODO: Could use result interface.rd into account if out of order completion is allowed.
   assign rf_waddr_wb_o  = ex_wb_pipe_i.rf_waddr;
   // TODO: Could use result interface.rd into account if out of order completion is allowed.
